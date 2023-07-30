aafc43df5a3df15e95de4dce7258ee1b3d0f3447
koreyoshilowers <67470907+koreyoshilowers@users.noreply.github.com>
2023-07-30 13:11:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.3.2/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 # git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
